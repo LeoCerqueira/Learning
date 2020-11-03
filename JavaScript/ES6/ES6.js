1cd65b8f5764d9a204b214c06019e740471f81ba
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-11-03 15:58:54 -0300

    ES

--- JavaScript/ES6/ES6.js
+++ JavaScript/ES6/ES6.js
@@ -1,1 +1,11 @@
-var = 'valor';
+//Anotações de aprendizados:
+
+//      01-tentar usar sempre o  let para declarar variáveis pois ele cobre o seu 
+//  local do escopo somente e não pode ser redaclarado
+
+
+//      02-usando a crase pra declarar strings podemos fazer um layout mais elegante
+// podemos usar o ${} para colocarmos uma variável dentro de uma sring tambem.
+
+// Práticas:
+// Usar let, crase para strings
