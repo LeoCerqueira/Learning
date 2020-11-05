e862327f84c6433097a821c622bacc815657d367
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-11-05 16:01:29 -0300

    preparing for webpack

---
+++ JavaScript/webpack/index.js
@@ -0,0 +1,10 @@
+let blackSquare = document.getElementById('blackSquare')
+let blueSquare = document.getElementById('blueSquare')
+
+
+let blackOut = () => blackSquare.style.visibility = 'hidden';
+let blueOut = () => blueSquare.style.visibility = 'hidden';
+
+let getBlackBack = () => blackSquare.style.visibility = 'visible';
+let getBlueBack = () => blueSquare.style.visibility = 'visible';
+
