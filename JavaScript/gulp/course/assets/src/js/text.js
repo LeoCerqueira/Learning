3b6be04848fb026bf6e21b92e6f9bdbc915262b5
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-11-09 15:27:42 -0300

    Imagemin Install

---
+++ JavaScript/gulp/course/assets/src/js/text.js
@@ -0,0 +1,7 @@
+let app = (function(document){
+
+    document.getElementById('logo').innerHTML = "Gulp";
+    document.getElementById('logoDesc').innerHTML = "Automate and enhance your workflow";
+    document.getElementById('video').innerHTML = "gulp-imagemin";
+
+})(document);
