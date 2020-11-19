180b54c048e898eb119926e58499b6244811fc9d
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-11-19 13:36:53 -0300

    Front-end masters

---
+++ FrontendMasters/JS/01/class.js
@@ -0,0 +1,18 @@
+// var born = new Date("March 2, 2021"); // using new will make objects representations 
+// console.log(born)
+// born.toUTCString();
+// console.log(born)
+
+//-----------Conver from one type to another 
+
+let age = 29;
+let statment = "";
+let frase = Number(statment) + age;
+console.log(frase)
+let result;
+
+if(statment){
+    result = statment;
+    console.log(result + "yeah!")
+
+}
