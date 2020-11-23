a5a9d3089a2bd3ae8305ad203425dc1d6fe3e8f5
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-11-23 15:45:04 -0300

    js

--- FrontendMasters/JS/01/class.js
+++ FrontendMasters/JS/01/class.js
@@ -1,18 +1,25 @@
-// var born = new Date("March 2, 2021"); // using new will make objects representations 
+// var born = new Date("March 2, 2021"); // using new will make objects representations
 // console.log(born)
 // born.toUTCString();
 // console.log(born)
 
-//-----------Conver from one type to another 
+//-----------Conver from one type to another
 
-let age = 29;
-let statment = "";
-let frase = Number(statment) + age;
-console.log(frase)
-let result;
+// let age = 29;
+// let statment = "";
+// let frase = Number(statment) + age;
+// console.log(frase)
+// let result;
 
-if(statment){
-    result = statment;
-    console.log(result + "yeah!")
+// if(statment){
+//     result = statment;
+//     console.log(result + "yeah!")
 
-}
+// }
+
+var leo = 'leo';
+(function anotherMe() {
+	var leo = 'Golumn';
+	console.log(leo);
+})();
+console.log(leo);
