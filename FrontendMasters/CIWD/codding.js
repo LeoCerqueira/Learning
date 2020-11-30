2e2729482c9c2426586307a3bd6f735784d72853
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-11-30 16:34:11 -0300

    js

--- FrontendMasters/CIWD/codding.js
+++ FrontendMasters/CIWD/codding.js
@@ -1,7 +1,15 @@
-const input = document.querySelector('.input-to-copy')
-const paragraph = document.querySelector('.p-to-copy-to')
+const input = document.querySelector('.input-to-copy');
+const paragraph = document.querySelector('.p-to-copy-to');
 
-input.addEventListener("keyup", function(){
-    paragraph.innerText = input.value
+input.addEventListener('keyup', function () {
+	paragraph.innerText = input.value;
 });
 
+document
+	.querySelector('.button-container')
+	.addEventListener('click', function (event) {
+		if (event.target.tagName === 'BUTTON') {
+			alert(`You clicked on button ${event.target.innerText}`);
+        }
+        event.stopPropagation();
+	});
