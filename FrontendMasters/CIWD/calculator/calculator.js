cd744fa3b3d19aa16408a31b31dae58b6e42ee3b
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-12-03 16:05:21 -0300

    calculator

--- FrontendMasters/CIWD/calculator/calculator.js
+++ FrontendMasters/CIWD/calculator/calculator.js
@@ -2,15 +2,19 @@ let result = document.querySelector('.result'); // screen
 let equal = 0;
 let tempVar = 0;
 let tempVar1 = 0;
+let confirmation =false;
 
 //----------add to the Screen
 
 document.querySelector('.numbers').addEventListener('click', function (event) {
+	
 	if (event.target.innerText.length == 1 && result.innerText == 0) {
+		
 		result.innerText = event.target.innerText;
 	} else if (event.target.innerText.length == 1 && result.innerText != 0) {
 		result.innerText = `${result.innerText}` + event.target.innerText;
 	}
+	
 });
 
 //----------------Clear
@@ -30,13 +34,40 @@ document.querySelector('.erase').addEventListener('click', function () {
 	}
 });
 
-document.querySelector('.plus').addEventListener('click', function () {
-	result.innerText = result.innerText + '+';
-	equal = result.innerText;
-	
+// document.querySelector('.plus').addEventListener('click', function () {
+// 	result.innerText = result.innerText + '+';
+// 	equal = result.innerText;
+// });
+
+document.querySelector('.aside').addEventListener('click', function (event) {
+	if (event.target.innerText.length == 1 && result.innerText != 0) {
+		let operator = event.target.innerText;
+		console.log(operator)
+		switch (operator) {
+			case "+":
+				result.innerText = result.innerText + '+';
+				break;
+			case "x":
+				result.innerText = result.innerText + '*';
+				break;
+
+			case "-":
+				result.innerText = result.innerText + '-';
+				break;
+			case "/":
+				result.innerText = result.innerText + '/';
+				break;
+
+		}
+
+		
+
+		
+	}
 });
 
 document.querySelector('.equal').addEventListener('click', function () {
-    equal = result.innerText;
-	result.innerText = eval(equal)
+	equal = result.innerText;
+	result.innerText = eval(equal).toFixed(2);
+	
 });
