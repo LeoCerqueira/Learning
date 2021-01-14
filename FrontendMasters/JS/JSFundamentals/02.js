6cf458823ac6854f75baf3b2f8eacd47663dd2b6
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-01-14 09:42:58 -0300

    js

---
+++ FrontendMasters/JS/JSFundamentals/02.js
@@ -0,0 +1,16 @@
+var game = {};
+game.murder = '??';
+game['weapons'] = [
+	{ type: 'lazers', location: 'lab' },
+	{ type: 'angry cats', location: 'room' },
+	{ type: 'dish soap', location: 'kichen' },
+]; // declaring an porperty
+
+game.name = [];
+game.name[0] = 'Miss Scarlet';
+game.name.push('Mr. Green');
+
+console.log(game.weapons[0]);
+
+var [first, last] = ['Leandro', 'Cerqueira'];
+console.log(first);
