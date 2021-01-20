64847f19ff100d0f81ccbc86be90ffec67d88209
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-01-20 12:30:39 -0300

    Tutorial

--- FrontendMasters/JS/JSFundamentals/filter.js
+++ FrontendMasters/JS/JSFundamentals/filter.js
@@ -1,22 +1,11 @@
 const _ = {};
 
-_.filter = function (arr, cb) {
-	const storage = [];
-	for (var i = 0; i < arr.lenght; i++) {
-		if (cb(arr[i], i, arr) === true) {
-			storage.push(arr[i]);
-		}
-	}
-	return storage;
-};
 
 _.filter = function (arr, cb) {
 	const storage = [];
 	_.each(arr, function (item, i, list) {
-		if (cb(item, i, list) === true) {
-			storage.push(item);
-		}
+		if (cb(item, i, list)) storage.push(item);
 	});
-	
+
 	return storage;
 };
