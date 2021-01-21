deab1e56882c0aca1a2bd9143a9be0c84659e377
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-01-21 10:42:50 -0300

    js

--- FrontendMasters/JS/JSFundamentals/filter.js
+++ FrontendMasters/JS/JSFundamentals/filter.js
@@ -1,5 +1,16 @@
 const _ = {};
 
+_.each = function (list, callback) {
+	if (Array.isArray(list)) {
+		for (var i = 0; i < list.length; i++) {
+			callback(list[i], i, list);
+		}
+	} else {
+		for (var key in list) {
+			callback(list[key], key, list);
+		}
+	}
+};
 
 _.filter = function (arr, cb) {
 	const storage = [];
@@ -9,3 +20,89 @@ _.filter = function (arr, cb) {
 
 	return storage;
 };
+_.map = function (list, callback) {
+	var storage = [];
+	// for (var i = 0; i < list.length; i++) {
+	// 	storage.push(callback(list[i], i, list));
+    // }
+    
+    _.each(list, function(v,i,list){
+        storage.push(callback(v,i,list))
+    })
+
+	return storage;
+};
+//definition
+//arrow functions
+
+const videoData = [
+	{
+		name: 'Miss Scarlet',
+		present: true,
+		rooms: [
+			{ kitchen: false },
+			{ ballroom: false },
+			{ conservatory: false },
+			{ 'dining room': false },
+			{ 'billiard room': false },
+			{ library: false },
+		],
+	},
+	{
+		name: 'Mrs. White',
+		present: false,
+		rooms: [
+			{ kitchen: false },
+			{ ballroom: false },
+			{ conservatory: false },
+			{ 'dining room': false },
+			{ 'billiard room': false },
+			{ library: false },
+		],
+	},
+	{
+		name: 'Reverend Green',
+		present: true,
+		rooms: [
+			{ kitchen: false },
+			{ ballroom: false },
+			{ conservatory: false },
+			{ 'dining room': false },
+			{ 'billiard room': false },
+			{ library: false },
+		],
+	},
+	{
+		name: 'Rusty',
+		present: false,
+		rooms: [
+			{ kitchen: false },
+			{ ballroom: false },
+			{ conservatory: false },
+			{ 'dining room': false },
+			{ 'billiard room': false },
+			{ library: false },
+		],
+	},
+	{
+		name: 'Colonel Mustard',
+		present: true,
+		rooms: [
+			{ kitchen: false },
+			{ ballroom: false },
+			{ conservatory: false },
+			{ 'dining room': false },
+			{ 'billiard room': false },
+			{ library: false },
+		],
+	},
+];
+
+const suspects = _.filter(videoData, function (suspectObjact) {
+	return suspectObjact.present;
+});
+
+const suspectsName = _.map( suspects, suspect =>{
+	return suspect.name
+})
+
