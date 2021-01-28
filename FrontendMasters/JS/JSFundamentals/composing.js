db77cf9fd4802c89e68afdc15e5cd2b2285723c2
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-01-28 09:34:45 -0300

    ja

---
+++ FrontendMasters/JS/JSFundamentals/composing.js
@@ -0,0 +1,7 @@
+const compose  = (fn, fn2) => {
+    return (arg) => {
+        const result = fn2(arg);
+        return fn(result);
+
+    }
+}
--- FrontendMasters/JS/JSFundamentals/test.js
+++ FrontendMasters/JS/JSFundamentals/test.js
@@ -1,12 +1,28 @@
-function countClues() {
-	var n = 0;
-	return {
-		count: function () {
-			return ++n;
-		},
+// const test = {
+// 	family: [
+// 		{ name: 'Léo', age: '29' },
+// 		{ name: 'Jú', age: '30' },
+// 		{ name: 'André', age: '32' },
+// 	],
+// 	work: [{ person1: 'radom1' }, { person1: 'radom1' }, { person1: 'radom1' }],
+// };
 
-		reset: function () {
-			return (n = 0);
-		},
-	};
-}
+// //console.log(test.work.length)
+
+// for( let i = 0 ; i<test.family.length; i++){
+
+
+// 	for(let key in test.family[i]){
+// 		console.log(test.family[i])
+// 	}
+// }
+
+var obj = { // does not create a new scope
+	i: 10,
+	b: () => console.log(this.i, this),
+	c: function() {
+	  console.log(this.i, this);
+	}
+  }
+
+console.log(obj.c())
