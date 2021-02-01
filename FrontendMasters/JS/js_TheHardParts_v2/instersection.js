fa2b4c1ef80e54013d5f1e801a7f8e95bc2737bd
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-01 12:06:04 -0300

    desafios

---
+++ FrontendMasters/JS/js_TheHardParts_v2/instersection.js
@@ -0,0 +1,43 @@
+// function instersection(arrays){
+//     for(let i = 0; i<arrays.length ; i++){
+//         for(let j = 0; j<arrays.arrays[i] ; j++)
+//             if(arrays[i][j] == arrays[i+1])
+
+//     }
+
+// }
+const arrays = [
+	[5, 10, 15, 20],
+	[15, 88, 1, 5, 7],
+	[1, 10, 15, 5, 20],
+];
+
+function comparison(arrays) {
+	var tempArr = [];
+
+	var finalArray = [];
+
+	for (let i = 0; i < arrays.length; i++) {
+		for (let j = 0; j < arrays[i].length; j++) {
+			tempArr.push(arrays[i][j]);
+		}
+	}
+
+	for (var k = 0; k < tempArr.length; k++) {
+		var atualNumber = tempArr[k];
+		var counter = 0;
+
+		for (let m = 0; m < tempArr.length; m++) {
+			if (atualNumber == tempArr[m]) {
+				counter = counter + 1;
+			}
+			if (counter == arrays.length) {
+				finalArray.push(atualNumber);
+			}
+		}
+	}
+	var newArr = [...new Set(finalArray)];
+	return newArr;
+}
+
+console.log(comparison(arrays));
---
+++ FrontendMasters/JS/js_TheHardParts_v2/union.js
@@ -0,0 +1,19 @@
+const mainArr = [
+	[5, 10, 15],
+	[15, 88, 1, 5, 7],
+	[100, 15, 10, 1, 5],
+];
+
+function union(arrays) {
+	var tempArray = [];
+
+	for (let i = 0; i < arrays.length; i++) {
+		for (let j = 0; j < arrays[i].length; j++) {
+			tempArray.push(arrays[i][j]);
+		}
+    }
+    var finalArray = [...new Set(tempArray)]
+    console.log(finalArray.sort(function(a, b){return a-b}));
+}
+
+union(mainArr)
