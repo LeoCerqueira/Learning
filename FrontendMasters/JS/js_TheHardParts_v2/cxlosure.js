4193c6817029050c5dbe3d583a274b979dfd08d6
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-03 10:22:49 -0300

    js

---
+++ FrontendMasters/JS/js_TheHardParts_v2/cxlosure.js
@@ -0,0 +1,15 @@
+const { Console } = require('console');
+
+function outer() {
+	let counter = 0;
+	function incrementCounter() {
+		counter++;
+	}
+	return incrementCounter;
+}
+
+const myNewFunction = outer();
+myNewFunction();
+myNewFunction();
+
+
