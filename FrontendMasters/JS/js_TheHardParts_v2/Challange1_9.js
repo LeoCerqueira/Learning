7efdc3aca40a01af88f69bd0f95a2559ed0e1dcf
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-08 09:56:11 -0300

    js

---
+++ FrontendMasters/JS/js_TheHardParts_v2/Challange1_9.js
@@ -0,0 +1,26 @@
+function objOfMatches(array1, array2, callback) {
+	callback(array1, array2);
+}
+
+function newObj(arr1, arr2) {
+
+    obj
+	
+
+	
+
+	for (let i = 0; i < arr1.length; i++) {
+
+        new Object
+		
+	}
+    
+}
+
+console.log(
+	objOfMatches(
+		['hi', 'howdy', 'bye', 'later', 'hello'],
+		['HI', 'Howdy', 'BYE', 'LATER', 'hello'],
+		newObj
+	)
+);
