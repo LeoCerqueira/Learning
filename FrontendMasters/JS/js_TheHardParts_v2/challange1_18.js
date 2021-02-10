4366eb09cb501610a24198b70d621a1199b5526a
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-10 10:01:33 -0300

    challenge1_18

---
+++ FrontendMasters/JS/js_TheHardParts_v2/challange1_18.js
@@ -0,0 +1,23 @@
+function objFilter(obj, callback) {
+
+    newObj = {};
+
+    for (var key in obj) {
+
+        if(callback(key) == obj[key]){
+            newObj[key] = obj[key]
+        }
+        
+    }
+
+    return newObj;
+
+}
+
+/*** Uncomment these to check your work! ***/
+const startingObj = {};
+startingObj[6] = 3;
+startingObj[2] = 1;
+startingObj[12] = 4;
+const half = n => n / 2;
+console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
