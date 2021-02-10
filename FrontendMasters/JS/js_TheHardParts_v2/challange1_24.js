5c849b8e3f7cd03c6af445e09c103fbcdf08fdac
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-10 12:09:55 -0300

    challenge 23 24

---
+++ FrontendMasters/JS/js_TheHardParts_v2/challange1_24.js
@@ -0,0 +1,19 @@
+// Challenge 24
+function myForEach(array, callback) {
+	var finalValue;
+	for (let i = 0; i < array.length; i++) {
+		finalValue = callback(array[i]);
+	}
+	return finalValue;
+}
+
+var sum = 0;
+
+function addToSum(num) {
+	sum += num;
+}
+
+/*** Uncomment these to check your work! ***/
+const nums = [1, 2, 3];
+myForEach(nums, addToSum);
+console.log(sum); // Should output 6
