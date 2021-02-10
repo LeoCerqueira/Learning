b541b16bb253fbe4f0f76daccfb3ff4b0afd34e3
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-10 09:03:46 -0300

    challenge15

---
+++ FrontendMasters/JS/js_TheHardParts_v2/challenge1_15.js
@@ -0,0 +1,27 @@
+function groupBy(array, callback) {
+	var newObj = {};
+	for (let index = 0; index < array.length; index++) {
+		newObj[callback(array[index])] = [];
+	}
+
+    
+
+	for (let index = 0; index < Object.keys(newObj).length; index++) {
+		for (let j = 0; j < array.length; j++) {
+
+            console.log(typeof(Number(Object.keys(newObj)[index])))
+
+			if (Number(Object.keys(newObj)[index]) == Math.floor(Number([array[j]]))) {
+				newObj[Object.keys(newObj)[index]].push(array[j]);
+			}
+		}
+	}
+
+	return newObj;
+}
+
+const decimals = [1.3, 2.1, 2.4];
+const floored = function (num) {
+	return Math.floor(num);
+};
+console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
