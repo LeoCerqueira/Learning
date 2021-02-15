af26172db246b62d76cb5628370ed378d8ac1e64
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-15 12:06:36 -0300

    Challenges ja

---
+++ FrontendMasters/JS/js_TheHardParts_v2/challenges2/15.js
@@ -0,0 +1,13 @@
+function russianRoulette(num) {
+
+    return function () {}
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const play = russianRoulette(3);
+// console.log(play()); // => should log 'click'
+// console.log(play()); // => should log 'click'
+// console.log(play()); // => should log 'bang'
+// console.log(play()); // => should log 'reload to play again'
+// console.log(play()); // => should log 'reload to play again'
