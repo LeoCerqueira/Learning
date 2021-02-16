b2f85878d781577a75988fdf6fd9c788a358e53f
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-16 14:18:04 -0300

    challenge15

--- FrontendMasters/JS/js_TheHardParts_v2/challenges2/15.js
+++ FrontendMasters/JS/js_TheHardParts_v2/challenges2/15.js
@@ -1,13 +1,25 @@
 function russianRoulette(num) {
+	var counter = 1;
 
-    return function () {}
+	return function () {
+		if (counter < num) {
+			counter++;
 
+			console.log('click');
+		} else if (counter == num) {
+			counter++;
+			console.log('bang');
+		} else {
+			counter++;
+			console.log('reload to play again');
+		}
+	};
 }
 
-// /*** Uncomment these to check your work! ***/
-// const play = russianRoulette(3);
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'bang'
-// console.log(play()); // => should log 'reload to play again'
-// console.log(play()); // => should log 'reload to play again'
+/*** Uncomment these to check your work! ***/
+const play = russianRoulette(3);
+play(); // => should log 'click'
+play(); // => should log 'click'
+play(); // => should log 'bang'
+play(); // => should log 'reload to play again'
+play(); // => should log 'reload to play again'
