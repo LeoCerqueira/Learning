3e7e991a4dfda92c5452c247b598a67d0b194af1
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-17 17:46:00 -0300

    challenges 01

---
+++ FrontendMasters/JS/js_TheHardParts_v2/challenges3/08.js
@@ -0,0 +1,8 @@
+function promised (val) {
+    // ADD CODE HERE
+  }
+  
+  // UNCOMMENT THESE TO TEST YOUR WORK!
+  // const createPromise = promised('wait for it...');
+  // createPromise.then((val) => console.log(val)); 
+  // will log "wait for it..." to the console after 2 seconds
