1d0aeee5714fe42ee55f8e5e3b539dce88ec410e
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-25 10:47:29 -0300

    js

--- FrontendMasters/JS/js_TheHardParts_v2/challenges2/7.js
+++ FrontendMasters/JS/js_TheHardParts_v2/challenges2/7.js
@@ -6,7 +6,7 @@ function rollCall(names) {
 }
 
 /*** Uncomment these to check your work! ***/
-const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth']);
+const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth','Andrea']);
 rollCaller(); // => should log 'Victoria'
 rollCaller(); // => should log 'Juan'
 rollCaller() // => should log 'Ruth'
