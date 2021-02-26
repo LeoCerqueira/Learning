9ec356b4fa74fa48dc9ad0f1824ec3d72f09515b
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-02-26 11:45:35 -0300

    js

--- FrontendMasters/JS/JS_fundations_v3/exercises/deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
+++ FrontendMasters/JS/JS_fundations_v3/exercises/deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
@@ -1,6 +1,23 @@
-function printRecords(recordIds) {
-	// TODO
+function getStudentFromId(studentId) {
+	return studentRecords.find(function machId(record) {
+		return record.id == studentId;
+	});
 }
+
+function printRecords(recordIds) {
+	var records = recordIds.map(getStudentFromId);
+
+	records.sort(function sortByNameAsc(record1,record2){
+		if (record1.name < record2.name) return -1;
+		else if (record1.name > record2.name) return 1;
+		else return 0;
+	});
+
+	records.forEach(function printRecord(record){
+		console.log(`${record.name} (${record.id}): ${record.paid ? "Paid" : "Not Paid"}`);
+	});
+}
+	
 
 function paidStudentsToEnroll() {
 	// TODO
@@ -10,28 +27,27 @@ function remindUnpaid(recordIds) {
 	// TODO
 }
 
-
 // ********************************
 
-var currentEnrollment = [ 410, 105, 664, 375 ];
+var currentEnrollment = [410, 105, 664, 375];
 
 var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+	{ id: 313, name: 'Frank', paid: true },
+	{ id: 410, name: 'Suzy', paid: true },
+	{ id: 709, name: 'Brian', paid: false },
+	{ id: 105, name: 'Henry', paid: false },
+	{ id: 502, name: 'Mary', paid: true },
+	{ id: 664, name: 'Bob', paid: false },
+	{ id: 250, name: 'Peter', paid: true },
+	{ id: 375, name: 'Sarah', paid: true },
+	{ id: 867, name: 'Greg', paid: false },
 ];
 
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 currentEnrollment = paidStudentsToEnroll();
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 remindUnpaid(currentEnrollment);
 
 /*
