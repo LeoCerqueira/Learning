65b79fbdb15e251e83cb16232ff55c20d2867deb
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-03-01 10:44:25 -0300

    prittier

--- FrontendMasters/JS/JS_fundations_v3/exercises/deep-js-foundations-v2-exercises/scope-exercises/hoisting/ex.js
+++ FrontendMasters/JS/JS_fundations_v3/exercises/deep-js-foundations-v2-exercises/scope-exercises/hoisting/ex.js
@@ -1,32 +1,29 @@
-
-var currentEnrollment = [ 410, 105, 664, 375 ];
+var currentEnrollment = [410, 105, 664, 375];
 
 var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+	{ id: 313, name: 'Frank', paid: true },
+	{ id: 410, name: 'Suzy', paid: true },
+	{ id: 709, name: 'Brian', paid: false },
+	{ id: 105, name: 'Henry', paid: false },
+	{ id: 502, name: 'Mary', paid: true },
+	{ id: 664, name: 'Bob', paid: false },
+	{ id: 250, name: 'Peter', paid: true },
+	{ id: 375, name: 'Sarah', paid: true },
+	{ id: 867, name: 'Greg', paid: false },
 ];
 
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 currentEnrollment = paidStudentsToEnroll();
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 remindUnpaid(currentEnrollment);
-
-
 
 function getStudentFromId(studentId) {
 	return studentRecords.find(matchId);
 
-	function matchId(record){
-		return (record.id == studentId);
+	function matchId(record) {
+		return record.id == studentId;
 	}
 }
 
@@ -37,14 +34,18 @@ function printRecords(recordIds) {
 
 	records.forEach(printRecord);
 
-	function sortByNameAsc(record1,record2){
+	function sortByNameAsc(record1, record2) {
 		if (record1.name < record2.name) return -1;
 		else if (record1.name > record2.name) return 1;
 		else return 0;
 	}
 
-	function printRecord(record){
-		console.log(`${record.name} (${record.id}): ${record.paid ? "Paid" : "Not Paid"}`);
+	function printRecord(record) {
+		console.log(
+			`${record.name} (${record.id}): ${
+				record.paid ? 'Paid' : 'Not Paid'
+			}`
+		);
 	}
 }
 
@@ -53,21 +54,21 @@ function paidStudentsToEnroll() {
 
 	var idsToEnroll = recordsToEnroll.map(getStudentId);
 
-	function needToEnroll(record){
-		return (record.paid && !currentEnrollment.includes(record.id));
+	function needToEnroll(record) {
+		return record.paid && !currentEnrollment.includes(record.id);
 	}
 
-	function getStudentId(record){
+	function getStudentId(record) {
 		return record.id;
 	}
 
-	return [ ...currentEnrollment, ...idsToEnroll ];
+	return [...currentEnrollment, ...idsToEnroll];
 }
 
 function remindUnpaid(recordIds) {
 	var unpaidIds = recordIds.filter(notYetPaid);
 
-	function notYetPaid(studentId){
+	function notYetPaid(studentId) {
 		var record = getStudentFromId(studentId);
 		return !record.paid;
 	}
@@ -75,9 +76,7 @@ function remindUnpaid(recordIds) {
 	printRecords(unpaidIds);
 }
 
-
 // ********************************
-
 
 /*
 	Bob (664): Not Paid
