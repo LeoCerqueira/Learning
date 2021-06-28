2852382f92a9872214a48a613939fcbce3182360
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-06-28 10:22:39 -0300

    Learning js

--- FrontendMasters/JS/Functional-Light JavaScript, v3/functional-light-v3/point-free/ex.js
+++ FrontendMasters/JS/Functional-Light JavaScript, v3/functional-light-v3/point-free/ex.js
@@ -1,14 +1,34 @@
-"use strict";
+'use strict';
 
-function output(txt) {
-	console.log(txt);
+// function output(txt) {
+// 	console.log(txt);
+// }
+
+var output = console.log.bind(console);
+var printIf = when(output);
+var isLongEnough = not(isShortEnough);
+
+function when(fn) {
+	return function (predicate) {
+		return function (...args) {
+			if (predicate(...args)) {
+				return fn(...args);
+			}
+		};
+	};
 }
 
-function printIf(shouldPrintIt) {
-	return function(msg) {
-		if (shouldPrintIt(msg)) {
-			output(msg);
-		}
+// function printIf(shouldPrintIt) {
+// 	return function (msg) {
+// 		if (shouldPrintIt(msg)) {
+// 			output(msg);
+// 		}
+// 	};
+// }
+
+function not(fn) {
+	return function nagetd(...args) {
+		return !fn(...args);
 	};
 }
 
@@ -16,14 +36,14 @@ function isShortEnough(str) {
 	return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
-}
+// function isLongEnough(str) {
+// 	return !isShortEnough(str);
+// }
 
-var msg1 = "Hello";
-var msg2 = msg1 + " World";
+var msg1 = 'Hello';
+var msg2 = msg1 + ' World';
 
-printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isLongEnough)(msg2); // Hello World
