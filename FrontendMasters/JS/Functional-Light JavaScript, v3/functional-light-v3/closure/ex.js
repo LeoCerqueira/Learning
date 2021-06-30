c1bba5da9560713a4b1a99213269f9460e59cadd
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-06-30 10:20:48 -0300

    jss

--- FrontendMasters/JS/Functional-Light JavaScript, v3/functional-light-v3/closure/ex.js
+++ FrontendMasters/JS/Functional-Light JavaScript, v3/functional-light-v3/closure/ex.js
@@ -12,7 +12,7 @@ function strBuilder(str) {
 var hello = strBuilder('Hello, ');
 var kyle = hello('Kyle');
 var susan = hello('Susan');
-var question = kyle('?')();
+var question = kyle('??')();
 var greeting = susan('!')();
 
 console.log(strBuilder('Hello, ')('')('Kyle')('.')('')() === 'Hello, Kyle.');
